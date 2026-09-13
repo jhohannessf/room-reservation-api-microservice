abd207dcc79997225cb2e370523226f5d174a558
jhohannessf <jhohannes.sf@gmail.com>
2026-09-13 01:47:04 -0300

    add novo microsservicos de server

---
+++ server-ms/src/test/java/br/com/jhohannesfreitas/server_ms/ServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhohannesfreitas.server_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
