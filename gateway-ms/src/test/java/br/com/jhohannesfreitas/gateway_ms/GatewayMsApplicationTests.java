5775f8b9c5d678c6e52c5f7ba44f38297b409b8f
jhohannessf <jhohannes.sf@gmail.com>
2026-09-13 01:46:33 -0300

    add novo microsservicos de gateway

---
+++ gateway-ms/src/test/java/br/com/jhohannesfreitas/gateway_ms/GatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhohannesfreitas.gateway_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
